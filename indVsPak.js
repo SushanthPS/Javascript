fc0bcd8faa6b4536fbc73851ed874bbd287742fd
Sushanth PS <35593841+sushanthps2@users.noreply.github.com>
2021-05-03 11:48:47 +0530

    Add files via upload

---
+++ indVsPak.js
@@ -0,0 +1,9 @@
+var scoreIndia = 320;
+var scorePak = 302;
+
+if(scoreIndia>scorePak)
+    console.log("Indian Wins");
+else if(scorePak>scoreIndia)
+    console.log("Pakistan Wins");
+else  
+    console.log("Match Tied");
---
+++ whoAmI.js
@@ -0,0 +1,18 @@
+var numOfSides = 5;
+
+if(numOfSides == 3)
+    console.log("Triangle");
+else if(numOfSides == 4)
+    console.log("Quadrilateral");
+else if(numOfSides == 5)
+    console.log("Pentagon");
+else if(numOfSides == 6)
+    console.log("Hexagon");
+else if(numOfSides == 7)
+    console.log("Heptagon");
+else if(numOfSides == 8)
+    console.log("Octagon");
+else if (numOfSides == 9)
+    console.log("Nonagon");
+else
+    console.log("Please refer google");
