159b930fba9d25830393c6f9d31809ab8177b0d5
Sushanth PS <35593841+sushanthps2@users.noreply.github.com>
2021-05-04 11:44:10 +0530

    Add files via upload

---
+++ leapYear.js
@@ -0,0 +1,14 @@
+var year = 2000;
+
+if (year % 100 == 0) {
+    if (year % 400 == 0)
+        console.log("Leap Year");
+    else
+        console.log("Not a Leap Year");
+} else {
+    if (year % 4 == 0)
+        console.log("Leap Year");
+    else
+        console.log("Not a Leap Year");
+
+}
