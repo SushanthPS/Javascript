154d56780ed51b87cdec4ee7a7701000d074ab85
Sushanth PS <35593841+sushanthps2@users.noreply.github.com>
2021-05-04 12:10:00 +0530

    Add files via upload

---
+++ dayOfTheWeek.js
@@ -0,0 +1,47 @@
+function runProgram(input) {
+    var a = Number(input);
+    switch (a) {
+        case 0:
+            console.log("Sunday");
+            break;
+        case 1:
+            console.log("Monday");
+            break;
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+    }
+
+}
+if (process.env.USERNAME === "getsu") {
+    runProgram(`5`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
